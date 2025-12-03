257cc3e26a5fa5fdd69bae42dcc2cf324ceaa0b8
vmohan074 <vmohan074@gmail.com>
2025-12-03 06:53:44 +0530

    Add files via upload

---
+++ pushover_handler.py
@@ -0,0 +1,15 @@
+import requests
+import os
+
+def send_pushover_notification(title, message, config):
+    app_token = os.getenv('PUSHOVER_APP_TOKEN')
+    user_key = os.getenv('PUSHOVER_USER_KEY')
+    url = 'https://api.pushover.net/1/messages.json'
+    payload = {
+        'token': app_token,
+        'user': user_key,
+        'title': title,
+        'message': message
+    }
+    response = requests.post(url, data=payload)
+    response.raise_for_status()
